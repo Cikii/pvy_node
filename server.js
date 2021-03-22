4d9a6f1a6f01b73178948111fcc80696d2c7b5fe
Cikii <cikislender@gmail.com>
2021-03-22 18:40:36 +0100

    v_alpha0.1

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = 5000 || process.env.PORT;
+app.use(express.static(path.join(__dirname, 'public')));
+app.listen(PORT, () => console.log(`Server uz bezi kamo ${PORT}`));
