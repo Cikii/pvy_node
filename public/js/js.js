982394b7b04cbd04e6e984fd0c7f026d9b3b4b70
Cikii <cikislender@gmail.com>
2021-03-22 23:31:30 +0100

    v1.0 + ukazky tweetu

---
+++ public/js/js.js
@@ -0,0 +1,17 @@
+const socket = io();
+let btn = document.getElementById("button");
+let zpravy = document.getElementById("zpravy");
+let pole = document.getElementById("status");
+let name = document.getElementById("fname");
+
+btn.addEventListener('click', function() {
+    if(name.value != "" && pole.value != "") {
+        socket.emit('message', pole.value, name.value);
+    } else {
+        alert("Nezadal jste jmeno nebo status");
+    }
+});
+
+socket.on('message', (msg, name, date) => {
+    zpravy.innerHTML += "<div id='zprava' class='row'><div id='box-zleva' class='col-xs-6'><img src='./avatar2.jpg' height='150px'></div><div id='box-zprava' class='col-xs-6'><h1>"+name+"</h1><p>"+date+"</p><p>"+msg+"</p></div></div>";
+});     
--- server.js
+++ server.js
@@ -1,7 +1,26 @@
 const express = require('express');
+const socketio = require('socket.io');
+const http = require('http');
 const path = require('path');
+const fs = require("fs");
 
 const app = express();
-const PORT = 5000 || process.env.PORT;
+const server = http.createServer(app);
+const io = socketio(server);
+
+
 app.use(express.static(path.join(__dirname, 'public')));
-app.listen(PORT, () => console.log(`Server uz bezi kamo ${PORT}`));
+
+io.on('connection', (socket) => {
+    socket.on('message', (msg, name) => {
+      let date_ob = new Date(Date.now());
+      let dateout = date_ob.getHours() + ":" + date_ob.getMinutes() + " &nbsp; " + date_ob.getFullYear() + "." + (date_ob.getMonth() + 1) + "." + date_ob.getDate();
+      let omsg = JSON.stringify(msg).replace(/\\n/g, "<br>").replace(/"/g, "");
+      io.emit('message', omsg, name, dateout);
+    });
+});
+
+const PORT = process.env.PORT || 5000;
+server.listen(PORT, function() {
+  console.log(`Server listening on port ${PORT}`);
+});
